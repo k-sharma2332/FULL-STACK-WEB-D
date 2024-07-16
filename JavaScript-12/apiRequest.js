c49fe11e6c58f4240e47e06d35104ebb5ab295e7
Khushi Sharma <ls390010@gmail.com>
2024-07-16 12:20:14 +0530

    JavaScript

--- JavaScript-12/apiRequest.js
+++ JavaScript-12/apiRequest.js
@@ -25,6 +25,10 @@ async function getFacts(){
     let res = await fetch(url);
     let data = await res.json();
     console.log(data.fact);
+
+    let res2 = await fetch(url);
+    let data2 = await res.json();
+    console.log(data2.fact);
    }
    catch(e){
     console.log(e);
