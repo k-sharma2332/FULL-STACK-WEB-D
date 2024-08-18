36e5fccacf9e91bc5d0c1f6cebfb159556611878
Khushi Sharma <ls390010@gmail.com>
2024-08-19 00:19:16 +0530

    MAJOR-PROJECT

--- MAJOR-PROJECT/app.js
+++ MAJOR-PROJECT/app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const Listings = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+//ejs mate help to genrate templates
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -23,6 +25,10 @@ app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
 app.get("/", (req, res) => {
     res.send("Root is working");
 });
@@ -54,17 +60,17 @@ app.post("/listings",async(req,res) => {
 });
 
 // Edit route
-app.get("/listings/:id/edit",async(req,res) => {
+app.post("/listings/:id",async(req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing})
 });
 
 // update route
-app.put("/listings/:id", async(req,res) => {
+app.get("/listings/:id", async(req,res) => {
     let {id} = req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-   res.redirect("/listings")
+   res.redirect(`/listings ${id}`)
 });
 
 //delete route
